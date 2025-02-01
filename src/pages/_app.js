34c9b69da3c11cf378debdfc78086a6f78e0c130
erickcr1 <salasalvardoerickfernando@gmail.com>
2025-02-01 13:17:17 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9132199630673133" crossorigin="anonymous"></script>
+            <Component {...pageProps} />
+        </>
+    );
 }
